0fb191dbe61d0ea6663dc76b5ad41efaff1fb1bc
Kevin Chen <kevchen21@gmail.com>
2020-04-30 12:53:48 -0400

    Parameterized DB name

--- repositories/UserRepository.js
+++ repositories/UserRepository.js
@@ -7,7 +7,7 @@
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  database: 'Todos',
+  database: process.env.DB_NAME,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
@@ -16,12 +16,22 @@ const pool = new Pool({
   // connectionString: process.env.DB_CONNECT_STRING,
 });
 
+const userSchema = {
+  table: process.env.DB_TABLE || 'users',
+  id: process.env.COL_ID || 'id',
+  firstName: process.env.COL_FIRST_NAME || 'first_name',
+  lastName: process.env.COL_LAST_NAME || 'last_name',
+  username: process.env.COL_USERNAME || 'username',
+  passwordHash: process.env.COL_PASSWORD_HASH || 'password_hash',
+  passwordSalt: process.env.COL_PASSWORD_SALT || 'password_salt',
+};
+
 const UserRepository = {
   getByUsername: async (username) => {
     const results = await pool.query(
       `
-      SELECT * FROM "Users" 
-      WHERE "Username" = $1
+      SELECT * FROM "${userSchema.table}"
+      WHERE "${userSchema.username}" = $1
       `,
       [username],
     );
@@ -33,8 +43,12 @@ const UserRepository = {
     const { firstname, lastname, username, passwordHash, passwordSalt } = userDto;
     const results = await pool.query(
       `
-      INSERT INTO "Users"
-        ("FirstName", "LastName", "Username", "PasswordHash", "PasswordSalt")
+      INSERT INTO "${userSchema.table}"
+        ("${userSchema.firstName}", 
+        "${userSchema.lastName}", 
+        "${userSchema.username}", 
+        "${userSchema.passwordHash}", 
+        "${userSchema.passwordSalt}")
       VALUES
         ($1, $2, $3, $4, $5)
       `,
